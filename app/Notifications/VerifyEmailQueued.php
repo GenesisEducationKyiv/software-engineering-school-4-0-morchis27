d9a25f32d2a1c1c7dac240745887a1c1336910bd
Christian <c.morales@expoplatform.com>
2024-06-08 18:45:02 +0300

    hw-2: phpstan level 7 fixes

--- app/Notifications/VerifyEmailQueued.php
+++ app/Notifications/VerifyEmailQueued.php
@@ -12,13 +12,24 @@ class VerifyEmailQueued extends VerifyEmail implements ShouldQueue
 {
     use Queueable;
 
+    private const SUBJECT_STRING = 'Verify Email Address';
+    private const FIRST_LINE_STRING = 'Please click the button below to verify your email address.';
+    private const ACTION_STRING = 'Verify Email Address';
+    private const SECOND_LINE_STRING = 'If you did not subscribe for this exchange rate newsletter then no further action is required.';
+
+
     protected function buildMailMessage($url): MailMessage
     {
+        $subject = Lang::get(self::SUBJECT_STRING);
+        $firstLine = Lang::get(self::FIRST_LINE_STRING);
+        $action = Lang::get(self::ACTION_STRING);
+        $secondLine = Lang::get(self::SECOND_LINE_STRING);
+
         return (new MailMessage)
             ->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
-            ->subject(Lang::get('Verify Email Address'))
-            ->line(Lang::get('Please click the button below to verify your email address.'))
-            ->action(Lang::get('Verify Email Address'), $url)
-            ->line(Lang::get('If you did not subscribe for this exchange rate newsletter then no further action is required.'));
+            ->subject(is_array($subject) ? self::SUBJECT_STRING : $subject)
+            ->line(is_array($firstLine) ? self::FIRST_LINE_STRING : $firstLine)
+            ->action(is_array($action) ? self::ACTION_STRING : $action, $url)
+            ->line(is_array($secondLine) ? self::SECOND_LINE_STRING : $secondLine);
     }
 }
