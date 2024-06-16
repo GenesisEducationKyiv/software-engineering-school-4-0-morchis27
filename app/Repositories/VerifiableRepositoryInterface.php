8e2a3c7b5d4b5eb4c0e1c52de4cbe6cdb83e82bf
Christian <c.morales@expoplatform.com>
2024-06-16 22:29:10 +0300

    hw-3: fix for verification of subscribers phpstan checks

--- app/Repositories/VerifiableRepositoryInterface.php
+++ app/Repositories/VerifiableRepositoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Models\NotifiableInterface;
 use App\Models\Subscriber;
 
 interface VerifiableRepositoryInterface
@@ -10,11 +11,11 @@ interface VerifiableRepositoryInterface
      * @param Subscriber $subscriber
      * @return void
      */
-    public function verify(Subscriber $subscriber): void;
+    public function verify(NotifiableInterface $subscriber): void;
 
     /**
      * @param Subscriber $subscriber
      * @return bool
      */
-    public function isVerified(Subscriber $subscriber): bool;
+    public function isVerified(NotifiableInterface $subscriber): bool;
 }
